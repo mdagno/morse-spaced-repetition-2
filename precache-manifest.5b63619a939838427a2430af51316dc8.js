2bfe2d2e42aabeda5c5061d9d38beefebb6f70eb
mdagno <mdj.agno@gmail.com>
2020-07-21 21:26:00 -0400

    Updates

---
+++ precache-manifest.5b63619a939838427a2430af51316dc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b01278ace09a0b49b89",
+    "url": "/morse-spaced-repetition-2/static/css/main.79f2e4cb.chunk.css"
+  },
+  {
+    "revision": "4b01278ace09a0b49b89",
+    "url": "/morse-spaced-repetition-2/static/js/main.4b01278a.chunk.js"
+  },
+  {
+    "revision": "4d220c55f4b2815a4e7e",
+    "url": "/morse-spaced-repetition-2/static/js/runtime~main.4d220c55.js"
+  },
+  {
+    "revision": "0cc60d48de2c78a8277d",
+    "url": "/morse-spaced-repetition-2/static/js/2.0cc60d48.chunk.js"
+  },
+  {
+    "revision": "d13f85205c4aecd83e28f1f48344f8fd",
+    "url": "/morse-spaced-repetition-2/static/media/landing.d13f8520.jpeg"
+  },
+  {
+    "revision": "2c1821eaf6fb7a6ed3ffb2feb5a4fb9f",
+    "url": "/morse-spaced-repetition-2/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9f2615998d0b6de99ea18f853974e978.js"
+  "/morse-spaced-repetition-2/precache-manifest.5b63619a939838427a2430af51316dc8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/morse-spaced-repetition-2/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
